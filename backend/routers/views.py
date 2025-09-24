4f5cb2168aaf3a125918efcdad93619d37e44873
magician9472 <magician9472@naver.com>
2025-09-24 12:38:20 +0900

    fix: template 경로 변경

--- backend/routers/views.py
+++ backend/routers/views.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+import os
 
 router = APIRouter()
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
+
 templates = Jinja2Templates(directory="templates")
 
 @router.get("/binance", response_class=HTMLResponse)
